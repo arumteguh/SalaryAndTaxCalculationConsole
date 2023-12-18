a198c28520e36828477abc0409d24ce46db83539
arumteguh <arumteguh9@gmail.com>
2023-12-18 23:26:45 +0700

    Add project files.

---
+++ SalaryAndTaxCalculationConsole/Calculation.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryAndTaxCalculationConsole
+{
+    public class Calculation
+    {
+
+        public const double TIER_1_MAX = 60000000;
+        public const double TIER_2_MAX = 250000000;
+        public const double TIER_3_MAX = 500000000;
+
+        public const double BPJSK_CAP = 12000000;
+        public const double JP_COMPANY_CAP = 8754600;
+        public const double JP_EMPLOYEE_CAP = 9559600;
+
+        public const double FUNCTIONAL_COST_CAP = 120000000;
+        public const string BY_COMPANY = "COMPANY";
+        public const string BY_EMPLOYEE = "EMPLOYEE";
+
+        public const int CURRENT_MONTH = 1;
+
+        public static double Tax(double salary)
+        {
+            double tax = 0;
+            double tiertax;
+
+            if (salary < 0) {
+                return 0;
+            }
+
+
+            if (salary > TIER_3_MAX)
+            {
+                tiertax = (salary - TIER_3_MAX) * 0.30;
+                tax += tiertax;
+                salary = TIER_3_MAX;
+                Console.WriteLine($"Tier 4: {tiertax.ToString("N2")}");
+            }
+
+            if (salary > TIER_2_MAX)
+            {
+                tiertax = (salary - TIER_2_MAX) * 0.25;
+                tax += tiertax;
+                salary = TIER_2_MAX;
+                Console.WriteLine($"Tier 3: {tiertax.ToString("N2")}");
+            }
+
+            if (salary > TIER_1_MAX)
+            {
+                tiertax = (salary - TIER_1_MAX) * 0.15;
+                tax += tiertax;
+                salary = TIER_1_MAX;
+                Console.WriteLine($"Tier 2: {tiertax.ToString("N2")}");
+            }
+
+            if (salary <= TIER_1_MAX)
+            {
+                tiertax = salary * 0.05;
+                tax += tiertax;
+                Console.WriteLine($"Tier 1: {tiertax.ToString("N2")}");
+            }
+
+            return tax;
+        }
+
+        //Q: Get percentage by PKP or Annual Salary?
+        public static double GetGrossUpPercentage(double value)
+        {
+            if (value > TIER_3_MAX)
+            {
+                //30%
+                return 0.30;
+            }
+
+            if (value > TIER_2_MAX)
+            {
+                //25%
+                return 0.25;
+            }
+
+            if (value > TIER_1_MAX)
+            {
+                //15%
+                return 0.15;
+            }
+
+            if (value <= TIER_1_MAX)
+            {
+                //5%
+                return 0.05;
+            }
+
+            return 0;
+        }
+
+        public static double BIK(List<Allowance> allowances)
+        {
+            double grossUp = 0;
+
+            foreach (Allowance allowance in allowances)
+            {
+                if (!allowance.IsBIK)
+                    continue;
+
+                if (allowance.PaymentSchedule == "YEARLY")
+                    allowance.Amount = allowance.Amount * 12;
+
+
+            }
+
+            return grossUp;
+        }
+
+        public static double GrossUpNatura(double BIK, double value)
+        {
+            double percentage = GetGrossUpPercentage(value);
+            return BIK / (1 - percentage) - BIK;
+        }
+        
+
+        public static Dictionary<string, double> PTKP = new Dictionary<string, double>()
+        {
+            {"TK0", 54000000},
+            {"TK1", 58500000},
+            {"TK2", 63000000},
+            {"TK3", 67500000},
+            {"K0", 58500000},
+            {"K1", 63000000},
+            {"K2", 67500000},
+            {"K3", 72000000}
+        };
+
+        public static double JKK(double salary)
+        {
+            //0.24%
+            return 0.0024 * salary;
+        }
+
+        public static double JKM(double salary)
+        {
+            //0.3%
+            return 0.003 * salary;
+        }
+
+        public static double BPJSK(double salary, bool IsJP, string by)
+        {
+            if (!IsJP)
+                return 0;
+
+            if (by == "COMPANY") //4%
+                return 0.04 * (salary <= BPJSK_CAP ? salary : BPJSK_CAP);
+
+            if (by == "EMPLOYEE") //1%
+                return 0.01 * (salary <= BPJSK_CAP ? salary : BPJSK_CAP);
+
+            return 0;
+        }
+
+        public static double JHT(double salary)
+        {
+            //2%
+            return 0.02 * salary;
+        }
+
+        public static double JP(double salary, bool IsJHT, string by)
+        {
+            
+            if (!IsJHT) 
+                return 0;
+
+            if (by == "COMPANY") //2% (source: https://glints.com/id/lowongan/perhitungan-bpjs-ketenagakerjaan/#:~:text=Nah%2C%20besar%20iuran%20dari%20JP,754.600%2C%20alias%20sisanya%20tidak%20dihitung.)
+                return 0.02 * (salary <= JP_COMPANY_CAP ? salary : JP_COMPANY_CAP);
+
+            if (by == "EMPLOYEE") //1%
+                return 0.01 * (salary <= JP_EMPLOYEE_CAP ? salary : JP_EMPLOYEE_CAP);
+
+            return 0;
+        }
+
+
+        public static double FunctionalCost(double grossSalary, int currentMonth)
+        {
+            return grossSalary >= FUNCTIONAL_COST_CAP ? 6000000 : 0.05 * grossSalary * 12 / currentMonth;
+        }
+
+        public static double Annual(double amount, int currentMonth)
+        {
+            return amount * 12 / currentMonth;
+        }
+
+
+
+    }
+}
---
+++ SalaryAndTaxCalculationConsole/Program.cs
@@ -0,0 +1,154 @@
+﻿using SalaryAndTaxCalculationConsole;
+
+const string FORMAT_N2 = "N2";
+
+var _IsBIK = true;
+
+var emp = new Employee() {
+    Name = "Arum Teguh Prayogo",
+    Salary = 7800000,
+    Status = "TK0",
+    IsJPCompany = true,
+    IsJKM = true,
+    IsJHT = true,
+    IsJPEmployee = true,
+    Allowances = { new Allowance() { Name = "Premi Askes", Amount = 120441.66666666666666666666666667, IsBIK = _IsBIK, PaymentSchedule = "MONTHLY"},
+    new Allowance() { Name = "Premi Life", Amount = 120441.66666666666666666666666667, IsBIK = _IsBIK, PaymentSchedule = "MONTHLY"},
+    new Allowance() { Name = "Medical", Amount = 120441.66666666666666666666666667, IsBIK = _IsBIK, PaymentSchedule = "MONTHLY"},
+    }
+    //Allowances = { new Allowance() { Name = "Premi Askes", Amount = 120442, IsBIK = true, PaymentSchedule = "MONTHLY"},
+    //new Allowance() { Name = "Premi Life", Amount = 120442, IsBIK = true, PaymentSchedule = "MONTHLY"},
+    //new Allowance() { Name = "Medical", Amount = 120442, IsBIK = true, PaymentSchedule = "MONTHLY"},
+    //}
+};
+
+
+
+emp.CalculatAnnualSalary();
+
+//var tax = Calculation.Tax(emp.AnnualSalary, emp.Status);
+
+Console.WriteLine("---------- DETAIL ----------");
+Console.WriteLine($"Salary: {emp.Salary.ToString(FORMAT_N2)}");
+
+//TODO: Probably change this, to handle the YEARLY Payment Cases?
+var BIK = emp.Allowances.Where(x => x.IsBIK).Sum(allowance => allowance.Amount);
+var AnnualBIK = Calculation.Annual(BIK, Calculation.CURRENT_MONTH);
+var AnnualGrossUp = Calculation.GrossUpNatura(AnnualBIK, emp.AnnualSalary);
+var GrossUp = AnnualGrossUp / 12;
+Console.WriteLine($"BIK: {BIK.ToString(FORMAT_N2)}");
+Console.WriteLine($"GrossUp: {GrossUp.ToString(FORMAT_N2)}");
+Console.WriteLine($"AnnualSalary: {emp.AnnualSalary.ToString(FORMAT_N2)}");
+Console.WriteLine($"AnnualBIK: {AnnualBIK.ToString(FORMAT_N2)}");
+Console.WriteLine($"AnnualGrossUp: {AnnualGrossUp.ToString(FORMAT_N2)}");
+var PTKP = Calculation.PTKP[emp.Status];
+Console.WriteLine($"Non-Taxable Income(PTKP): {emp.Status} --> {PTKP.ToString(FORMAT_N2)}");
+
+Console.WriteLine("---------- BPJS ----------");
+var JKK = Calculation.JKK(emp.Salary);
+var JKM  = Calculation.JKM(emp.Salary);
+//Q1: Why this JPCompany = 0 in the sheets?
+var JPCompany = Calculation.JP(emp.Salary, emp.IsJHT, Calculation.BY_COMPANY);
+var BPJSKCompany = Calculation.BPJSK(emp.Salary, emp.IsJPCompany, Calculation.BY_COMPANY);
+var BPJSPaidByCompany = JKK + JKM + BPJSKCompany;
+var AnnualBPJSPaidByCompany = Calculation.Annual(BPJSPaidByCompany, Calculation.CURRENT_MONTH);
+Console.WriteLine($"JKK: {JKK.ToString(FORMAT_N2)}");
+Console.WriteLine($"JKM: {JKM.ToString(FORMAT_N2)}");
+Console.WriteLine($"JPCompany: {JPCompany.ToString(FORMAT_N2)}");
+Console.WriteLine($"BPJSK: {BPJSKCompany.ToString(FORMAT_N2)}");
+Console.WriteLine($"BPJSPaidByCompany: {BPJSPaidByCompany.ToString(FORMAT_N2)}");
+Console.WriteLine($"AnnualBPJSPaidByCompany: {AnnualBPJSPaidByCompany.ToString(FORMAT_N2)}");
+
+var GrossSalary = emp.Salary + BIK + GrossUp + BPJSPaidByCompany;
+var AnnualGrossSalary = Calculation.Annual(GrossSalary, Calculation.CURRENT_MONTH);
+var FunctionalCost = Calculation.FunctionalCost(AnnualGrossSalary, 12);
+Console.WriteLine($"GrossSalary: {GrossSalary.ToString(FORMAT_N2)}");
+Console.WriteLine($"AnnualGrossSalary: {AnnualGrossSalary.ToString(FORMAT_N2)}");
+Console.WriteLine($"FunctionalCost: {FunctionalCost.ToString(FORMAT_N2)}");
+
+Console.WriteLine("====================");
+
+var JHT = Calculation.JHT(emp.Salary);
+var JPEmployee = Calculation.JP(emp.Salary, emp.IsJHT, Calculation.BY_EMPLOYEE);
+var BPJSKEmployee = Calculation.BPJSK(emp.Salary, emp.IsJPEmployee, Calculation.BY_EMPLOYEE);
+var BPJSPaidByEmployee = JHT + JPEmployee;
+var AnnualBPJSPaidByEmployee = Calculation.Annual(BPJSPaidByEmployee, Calculation.CURRENT_MONTH);
+Console.WriteLine($"JHT: {JHT.ToString(FORMAT_N2)}");
+Console.WriteLine($"JPEmployee: {JPEmployee.ToString(FORMAT_N2)}");
+Console.WriteLine($"BPJSKEmployee: {BPJSKEmployee.ToString(FORMAT_N2)}");
+Console.WriteLine($"BPJSPaidByEmployee: {BPJSPaidByEmployee.ToString(FORMAT_N2)}");
+Console.WriteLine($"AnnualBPJSPaidByEmployee: {AnnualBPJSPaidByEmployee.ToString(FORMAT_N2)}");
+
+var GrossIncome = GrossSalary - BPJSPaidByEmployee;
+//var AnnualGrossIncome = Calculation.Annual(GrossIncome, Calculation.CURRENT_MONTH);
+//Q2: why the GrossIncome in the sheets is (AnnualGrossSalary - FunctionalCost - AnnualBPJSPaidByEmployee)
+var AnnualGrossIncome = AnnualGrossSalary - FunctionalCost - AnnualBPJSPaidByEmployee;
+Console.WriteLine($"GrossIncome: {GrossIncome.ToString(FORMAT_N2)}");
+
+Console.WriteLine($"AnnualGrossIncome: {AnnualGrossIncome.ToString(FORMAT_N2)}");
+
+
+var PKP = AnnualGrossIncome - PTKP;
+PKP = Math.Floor(PKP / 1000) * 1000;
+Console.WriteLine($"Taxable Income(PKP): {PKP.ToString(FORMAT_N2)}");
+
+
+var AnnualTax = Calculation.Tax(PKP);
+var MonthlyTax = AnnualTax / 12 * Calculation.CURRENT_MONTH;
+
+//Console.WriteLine($"Taxable: {(emp.AnnualSalary - Calculation.PTKP[emp.Status]).ToString(FORMAT_N2)}");
+Console.WriteLine($"Tax: {MonthlyTax.ToString(FORMAT_N2)}");
+Console.WriteLine($"AnnualTax: {AnnualTax.ToString(FORMAT_N2)}");
+
+Console.WriteLine("---------- RESULT ----------");
+
+
+var TotalTax = MonthlyTax;
+var Net = emp.Salary + GrossUp - (BPJSPaidByEmployee + BPJSKEmployee + MonthlyTax);
+Console.WriteLine($"Monthly Tax: {MonthlyTax.ToString(FORMAT_N2)}");
+Console.WriteLine($"Total Tax: {TotalTax.ToString(FORMAT_N2)}");
+Console.WriteLine($"Net: {Net.ToString(FORMAT_N2)}");
+
+
+
+
+
+
+
+public class Employee
+{
+    public string? Name { get; set; }
+    public double Salary { get; set; }
+
+    public double AnnualSalary { get; set; }
+
+    public string? Status { get; set; }
+
+    public bool IsJPCompany { get; set; }
+
+    public bool IsJKM { get; set; }
+
+    public bool IsJHT { get; set; }
+    
+    public bool IsJPEmployee { get; set; }
+
+    public List<Allowance> Allowances { get; set; } = new List<Allowance>();    
+    public Employee() { }
+
+    public void CalculatAnnualSalary()
+    {
+        this.AnnualSalary = Calculation.Annual(this.Salary, Calculation.CURRENT_MONTH);
+    }
+}
+
+public class Allowance
+{
+    public string? Name { get; set; }
+    public double Amount { get; set;}
+
+    public bool IsBIK { get; set; }
+
+    public string? PaymentSchedule { get; set; }
+
+    public Allowance() { }
+}
